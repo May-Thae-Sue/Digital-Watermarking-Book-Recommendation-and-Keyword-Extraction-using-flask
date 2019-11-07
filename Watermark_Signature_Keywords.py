4491bffea7d5237b37d691f9f828518f02c616dc
zawlinnnaing <zawlinnnaing0018@gmail.com>
2019-11-08 01:15:43 +0630

    finishing up

--- Watermark_Signature_Keywords.py
+++ Watermark_Signature_Keywords.py
@@ -184,55 +184,46 @@ def recommendation():
     # Function to get the most similar books
 
     # print(check)
-    # input the index of the book and get top 10 book recommendation
+    #input the index of the book and get top 10 book recommendation
     row_no = str(check[check['book_id'] == num].index.values)
     row_no = int(row_no.replace("[", "").replace("]", ""))
-    # print("THIS IS ROW NO")
-    # print(row_no)
-    #   row_no = int(row_no.replace("[","").replace("]","")) ## chosen id from web
     # print("Index values for recommendation",row_no)
-    recom = recommend(row_no, cosine_similarity, indices, df)  ## need to edit, chosen id from web instead of 1
+    # need to edit, chosen id from web instead of 1
+    recom = recommend(row_no, cosine_similarity, indices, df, check)
 
     book_id = []
-    # for book title
     book_title = []
-    for book in recom:
-        book_title.append(book)
-    # print("title",book_title)
-    check = check.set_index('title')
-    for i in range(len(book_title)):
-        book_id.append(int(check.loc[book_title[i], 'book_id']))  # for book Id
-    # print("book_id",book_id)
+    for id in recom:
+        book_id.append(id)
+    check = check.set_index('book_id')
+    for i in range(len(book_id)):
+        book_title.append(check.loc[book_id[i], 'title'])  # for book Id
 
     # for bookId and Title json
     output_json = []
-    # inside = {}
-    for i in range(len(book_title)):
-        ##inside["book_id"] = book_id[i]
-        ##inside["book_title"] = book_title[i]
+    for i in range(len(book_id)):
         output_json.append({"book_id": book_id[i], "book_title": book_title[i]})
-    # print("json",output_json)
     return json.dumps(output_json)
-    # return "hello"
 
 
-def recommend(index, method, indices, df):
+def recommend(index, method, indices, df, check):
+
     id = indices[index]
-    # print("id",id)
+    #print("id",id)
     # Get the pairwise similarity scores of all books compared that book,
     # sorting them and getting top 5
     similarity_scores = list(enumerate(method[id]))
-    # print("sim",df)
-    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+    #print("sim",df)
+    similarity_scores = sorted(
+        similarity_scores, key=lambda x: x[1], reverse=True)
     similarity_scores = similarity_scores[1:6]
 
-    # Get the books index
+    #Get the books index
     books_index = [i[0] for i in similarity_scores]
 
-    # Return the top 10 most similar books using integar-location based indexing (iloc)
+    #Return the top 5 most similar books using integar-location based indexing (iloc)
     ##print("result",df['title'].iloc[books_index])
-    return df['title'].iloc[books_index]
-
+    return check['book_id'].iloc[books_index]
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port="5000", debug=True)
